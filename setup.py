cc1d0cefea51c9e1f8b51c5bc245871332e55e1e
Bernd Biskup <bbiskup@gmx.de>
2014-12-11 21:00:15 +0100

    Basic project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='purkinje',
+      version='0.0.1',
+      description='Test runner for py.test with web GUI',
+      author='Bernhard Biskup',
+      author_email='bbiskup@gmx.de',
+      url='biskup-software.de',
+      )
---
+++ src/purkinje.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import flask
+
+
+if __name__ == '__main__':
+    print('purkinje')
