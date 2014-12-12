2e5b41a215e484cf46fb8a93783e568471fe54cd
Bernd Biskup <bbiskup@gmx.de>
2014-12-12 09:23:58 +0100

    Integrated flask-assets; created basic bundle

---
+++ src/purkinje/assets.py
@@ -0,0 +1,12 @@
+from flask.ext.assets import Bundle, Environment
+
+
+bundles = {
+    'home_css': Bundle(
+        'css/default.css'
+    )
+}
+
+def register_assets(app):
+    assets = Environment(app)
+    assets.register(bundles)
