1c0968eb7c3ea97c208b6d245cc4669f15119d4f
Bernd Biskup <bbiskup@gmx.de>
2014-12-12 07:41:31 +0100

    barebone flask app

--- src/purkinje.py
+++ src/purkinje.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
-import flask
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "Purkinje"
 
 if __name__ == '__main__':
-    print('purkinje')
+    print('purkinje ready')
+    app.run()
