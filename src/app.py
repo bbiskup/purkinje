68a475b28e674ceef859387cab3739be70e7b7be
Bernd Biskup <bbiskup@gmx.de>
2014-12-12 08:00:23 +0100

    Added Flask-Sockets package

--- src/app.py
+++ src/app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sockets import Sockets
 
 app = Flask(__name__)
+sockets = Sockets(app)
 
 
 @app.route('/')
 def main():
     return "Purkinje"
+
+
+@sockets.route('/subscribe')
+def subscribe(ws):
+    while True:
+        message = ws.receive()
+        ws.send('Response to "%s"', message)
