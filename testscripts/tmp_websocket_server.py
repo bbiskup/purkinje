e34c5a4253c140c44251aa1c5a89025ef07c767f
Bernd Biskup <bbiskup@gmx.de>
2014-12-14 14:51:05 +0100

    Added preliminary test scripts

---
+++ testscripts/tmp_websocket_server.py
@@ -0,0 +1,17 @@
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
+
+
+def websocket_app(environ, start_response):
+    print('Request for {}'.format(environ["PATH_INFO"]))
+    if environ["PATH_INFO"] == '/echo':
+        ws = environ["wsgi.websocket"]
+        print('ws: {}'.format(ws))
+
+        while True:
+            message = ws.receive()
+            ws.send(message)
+
+server = pywsgi.WSGIServer(("", 8000), websocket_app,
+                           handler_class=WebSocketHandler)
+server.serve_forever()
