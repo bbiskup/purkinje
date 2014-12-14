d32e01d229fdbddb77638da7e7ee5f46b61da99b
Bernd Biskup <bbiskup@gmx.de>
2014-12-14 14:54:13 +0100

    Added test case

--- src/purkinje/view_test.py
+++ src/purkinje/view_test.py
@@ -1,6 +1,9 @@
 import gevent.monkey
 gevent.monkey.patch_all()
+import urllib2
+from urlparse import urlparse
 import pytest
+import websocket
 import httplib
 from flask import url_for
 from app import get_app
@@ -32,3 +35,21 @@ def test_404(client):
 def test_trigger_error(client):
     assert client.get(
         '/trigger_error').status_code == httplib.INTERNAL_SERVER_ERROR
+
+
+# TODO: not possible with live server (needs WSGIServer with WebSocketHandler?)
+# #@pytest.mark.usefixtures('live_server')
+# def test_subscribe(live_server):
+#     # s = urllib2.urlopen(url_for('/subscribe2')).status_code
+#     # assert s == httplib.OK
+
+#     ws_url_base = live_server.url().replace('http', 'ws')
+
+#     #ws = websocket.create_connection('ws://127.0.0.1:5000/subscribe')
+#     view_url = url_for('subscribe2')
+#     full_url = '{}{}'.format(ws_url_base, view_url)
+#     ws = websocket.create_connection(full_url)
+
+
+def test_websocket_client(client):
+    assert client.get(url_for('websocket_client')).status_code == httplib.OK
