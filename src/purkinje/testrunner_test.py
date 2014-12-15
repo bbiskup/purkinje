934a237e0ff38eb0981c31d584e68d2369d31358
Bernhard Biskup <bbiskup@gmx.de>
2014-12-15 13:42:18 +0100

    New test case: excecutes py.test in temporary project directory

--- src/purkinje/testrunner_test.py
+++ src/purkinje/testrunner_test.py
@@ -1,11 +1,29 @@
 """ Tests for py.test test runner
 """
 
+import os
 import shutil
 import tempfile
 import pytest
+from conftest import TESTDATA_DIR
+from testrunner import PyTestRunner
 
 
 @pytest.fixture
-def project_dir(unique_filename):
-    result = ensure_dir()
+def testrunner():
+    return PyTestRunner()
+
+
+def test_empty_single_pass(tmpdir, testrunner):
+    test_proj_path = str(tmpdir) + '/singlepass'
+    shutil.copytree(TESTDATA_DIR + '/testproj/singlepass',
+                    test_proj_path)
+
+    orig_path = os.getcwd()
+    try:
+        os.chdir(test_proj_path)
+        test_result = testrunner.run(['simple_test.py'])
+        assert test_result == 0
+        assert len(testrunner.monitor_plugin.reports) == 2
+    finally:
+        os.chdir(orig_path)
