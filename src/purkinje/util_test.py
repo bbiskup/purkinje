4feef04afdca597044cc04922f799610757d9090
Bernhard Biskup <bbiskup@gmx.de>
2014-12-15 13:10:31 +0100

    Added test case

--- src/purkinje/util_test.py
+++ src/purkinje/util_test.py
@@ -4,6 +4,7 @@
 
 import pytest
 import os
+import errno
 from os.path import exists
 import stat
 import util as sut
@@ -15,3 +16,23 @@ def test_ensure_dir_new(unique_filename, tmpdir):
     sut.ensure_dir(path)
     assert exists(path)
     assert os.stat(path).st_mode & stat.S_IRWXU == stat.S_IRWXU
+
+
+@pytest.mark.parametrize('mode',
+                         [0,
+                          stat.S_IRUSR,
+                          stat.S_IWUSR,
+                          stat.S_IXUSR])
+def test_ensure_dir_new(mode, unique_filename, tmpdir):
+    path = str(tmpdir) + unique_filename
+    assert not exists(path)
+    os.mkdir(path)
+    assert exists(path)
+    os.chmod(path, mode)
+    try:
+        sut.ensure_dir(path)
+    except IOError as e:
+        assert e.errno == errno.EPERM
+    else:
+        assert False, 'Should have raised IOError'
+    assert exists(path)
