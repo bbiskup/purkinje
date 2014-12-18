5a1fae92b339e926a9168b4dc6c3c48a12e8b2a8
Bernhard Biskup <bbiskup@gmx.de>
2014-12-18 16:59:46 +0100

    Check syntax of .yml files in proj dir; fixed syntax of .travis.yml

--- src/purkinje/docformat_test.py
+++ src/purkinje/docformat_test.py
@@ -4,14 +4,15 @@
 """
 
 import os
+import yaml
 import pytest
 import restructuredtext_lint as rstlint
 from conftest import PROJ_DIR
 
 
-def proj_rst_files():
+def proj_files(suffix):
     for f in os.listdir(PROJ_DIR):
-        if f.endswith('.rst'):
+        if f.endswith(suffix):
             yield f
 
 
@@ -21,10 +22,17 @@ def _format_rst_lint_errors(errors):
             for x in errors]
 
 
-@pytest.mark.parametrize('filename', proj_rst_files())
+@pytest.mark.parametrize('filename', proj_files('.rst'))
 def test_rst_syntax(filename):
     lint_result = rstlint.lint_file(filename)
     error_msg = '{}: {}'.format(
         filename,
         _format_rst_lint_errors(lint_result))
     assert len(lint_result) == 0, error_msg
+
+
+@pytest.mark.parametrize('filename', proj_files('.yml'))
+def test_yaml_syntax(filename):
+    with open(filename) as f:
+        # coerce to force evaluation of generator
+        list(yaml.parse(f))
