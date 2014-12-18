0b9fbe27ca534368e60d3a8a001d464b1f27e5fe
Bernhard Biskup <bbiskup@gmx.de>
2014-12-18 13:06:59 +0100

    better error reporting in docformat test

--- src/purkinje/docformat_test.py
+++ src/purkinje/docformat_test.py
@@ -15,7 +15,16 @@ def proj_rst_files():
             yield f
 
 
+def _format_rst_lint_errors(errors):
+    return ['{}: {}'.format(x.line,
+                            x.full_message)
+            for x in errors]
+
+
 @pytest.mark.parametrize('filename', proj_rst_files())
 def test_rst_syntax(filename):
     lint_result = rstlint.lint_file(filename)
-    assert len(lint_result) == 0
+    error_msg = '{}: {}'.format(
+        filename,
+        _format_rst_lint_errors(lint_result))
+    assert len(lint_result) == 0, error_msg
