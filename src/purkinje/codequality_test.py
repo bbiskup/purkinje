78409e0ea649002a850293dda7b2ba80c9502cd7
Bernhard Biskup <bbiskup@gmx.de>
2014-12-18 17:26:35 +0100

    Added check for McCabe complexity to py.test

---
+++ src/purkinje/codequality_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Miscellaneous tests about code quality"""
+
+import subprocess
+
+MAX_MCCABE_COMPLEXITY = 10
+
+
+def test_mccabe():
+    cmd = 'flake8 --max-complexity=%d src' % MAX_MCCABE_COMPLEXITY
+    result = subprocess.call(cmd.split())
+    assert result == 0, 'McCabe check seems to have failed'
