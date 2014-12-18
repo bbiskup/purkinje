3863ad7d90b1050e973daa49d44a8c502f3617df
Bernd Biskup <bbiskup@gmx.de>
2014-12-18 12:28:36 +0100

    Added functions for ensuring deletion of file/dir  - to ensure correct execution of tests with Travis where    __pycache__ is not generated

--- src/purkinje/util.py
+++ src/purkinje/util.py
@@ -1,6 +1,8 @@
 import os
+import os.path as op
 import stat
 import errno
+import shutil
 
 
 def ensure_dir(path):
@@ -17,3 +19,37 @@ def ensure_dir(path):
         raise IOError(errno.EPERM,
                       'Insufficient directory permissions',
                       path)
+
+
+def _ensure_file_deleted(path):
+    try:
+        os.remove(path)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+
+
+def _ensure_dir_deleted(path):
+    try:
+        shutil.rmtree(path)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+
+
+def ensure_deleted(path):
+    if not op.exists(path):
+        return
+    if op.isfile(path):
+        _ensure_file_deleted(path)
+    elif op.isdir(path):
+        _ensure_dir_deleted(path)
+    else:
+        raise ValueError('Invalid file type: {}'.format(path))
+
+
+def a():
+    x = 3
+    print x
+    print x * 2
+    print x * 3
--- src/purkinje/util_test.py
+++ src/purkinje/util_test.py
@@ -5,16 +5,24 @@
 import pytest
 import os
 import errno
-from os.path import exists
+import os.path as op
 import stat
 import util as sut
 
 
+@pytest.fixture()
+def existent_empty_file(tmpdir):
+    filename = op.join(str(tmpdir), 'file_1.txt')
+    with open(filename, 'w'):
+        pass
+    return filename
+
+
 def test_ensure_dir_new(unique_filename, tmpdir):
     path = str(tmpdir) + unique_filename
-    assert not exists(path)
+    assert not op.exists(path)
     sut.ensure_dir(path)
-    assert exists(path)
+    assert op.exists(path)
     assert os.stat(path).st_mode & stat.S_IRWXU == stat.S_IRWXU
 
 
@@ -25,9 +33,9 @@ def test_ensure_dir_new(unique_filename, tmpdir):
                           stat.S_IXUSR])
 def test_ensure_dir_missing_perm(mode, unique_filename, tmpdir):
     path = str(tmpdir) + unique_filename
-    assert not exists(path)
+    assert not op.exists(path)
     os.mkdir(path)
-    assert exists(path)
+    assert op.exists(path)
     os.chmod(path, mode)
     try:
         sut.ensure_dir(path)
@@ -35,13 +43,33 @@ def test_ensure_dir_missing_perm(mode, unique_filename, tmpdir):
         assert e.errno == errno.EPERM
     else:
         assert False, 'Should have raised IOError'
-    assert exists(path)
+    assert op.exists(path)
 
 
 def test_ensure_dir_existing(unique_filename, tmpdir):
     path = str(tmpdir) + unique_filename
-    assert not exists(path)
+    assert not op.exists(path)
     os.mkdir(path)
-    assert exists(path)
+    assert op.exists(path)
     sut.ensure_dir(path)
-    assert exists(path)
+    assert op.exists(path)
+
+
+def test_ensure_deleted_nonexistent_file(tmpdir):
+    filename = op.join(str(tmpdir), '__NONEXISTANT')
+    sut.ensure_deleted(filename)
+    assert not op.exists(filename)
+
+
+def test_ensure_deleted_nonexistent_dir(tmpdir):
+    dirname = str(tmpdir)
+    sut.ensure_deleted(dirname)
+    assert not op.exists(dirname)
+
+
+def test_ensure_deleted_existent_file(existent_empty_file):
+    sut.ensure_deleted(existent_empty_file)
+
+
+def test_ensure_deleted_existent_dir(tmpdir):
+    sut.ensure_deleted(str(tmpdir))
