0118e2061c38ac6f71663a5120b667177005746b
Bernhard Biskup <bbiskup@gmx.de>
2014-12-18 09:44:02 +0100

    Added gevent monkey patching and new constant to conftest.py

--- src/purkinje/conftest.py
+++ src/purkinje/conftest.py
@@ -2,10 +2,17 @@
 
 """Common fixtures and test functions"""
 
+import gevent.monkey
+gevent.monkey.patch_all()
 import uuid
 import pytest
 
 TESTDATA_DIR = 'testdata'
+
+# Timeout to apply when waiting for actions that should
+# happen 'immediately', i.e. should take only a few
+# milliseconds
+QUASI_IMMEDIATE_TIMEOUT = .2
 
 
 @pytest.fixture
