d3edc46f08def3fef679b5aaf5052b1e25df72cd
Bernhard Biskup <bbiskup@gmx.de>
2014-12-20 21:22:18 +0100

    Switched to "nature" Sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,9 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 
 html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
