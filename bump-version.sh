b7a86be516a419de3e07cee0cb7a3597fe7c59cc
Bernhard Biskup <bbiskup@gmx.de>
2014-12-20 18:51:07 +0100

    Maintain __version__ with bump-version.sh

--- bump-version.sh
+++ bump-version.sh
@@ -4,5 +4,6 @@
 
 VERSION=$1
 
-sed -i -e"s/ version='..*',/ version='"$VERSION"""',/g" setup.py
-sed -i -e's/ "version": "..*",/ "version": "'$VERSION'",/g' package.json
+sed -i -e"s/ version='.*',/ version='"$VERSION"',/g" setup.py
+sed -i -e's/ "version": ".*",/ "version": "'$VERSION'",/g' package.json
+sed -i -e"s/__version__ = '.*'/__version__ = '"$VERSION"'/g" purkinje/__init__.py
---
+++ purkinje/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'Bernhard Biskup'
+__email__ = 'bbiskup@gmx.de'
+__version__ = '0.1.0'
