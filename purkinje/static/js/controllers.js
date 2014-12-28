e62c318c03b434a0da879edeedaaa866537a2521
Bernhard Biskup <bbiskup@gmx.de>
2014-12-28 15:38:01 +0100

    Refactoring: extracted functions

--- purkinje/static/js/controllers.js
+++ purkinje/static/js/controllers.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 app.controller('DummyController', function($scope) {
-    $scope.xyz = "hello";
-    //$scope.people = ['Jim', 'Jill', 'Jerome'];
+    $scope.xyz = "hello"
+    //$scope.people=['Jim', 'Jill', 'Jerome']
     $scope.people = [{
         name: 'Jim'
     }, {
@@ -12,24 +12,9 @@ app.controller('DummyController', function($scope) {
 });
 
 
-app.controller('TestResultsTableController', ['$scope', 'WebSocketService',
-
-    function($scope, WebSocketService) {
-        $scope.webSocketEvents = [];
-        $scope.dummyPayload = WebSocketService.registerClient();
-
-        $scope.$on('webSocketMsg', function(event, data) {
-            var start = new Date();
-            console.debug('$scope: webSocketMsg', data);
-            $scope.webSocketEvents.push(data);
-            if ($scope.webSocketEvents.length > defs.maxDummyMsgScopeLength) {
-                $scope.webSocketEvents = $scope.webSocketEvents.splice(1);
-            }
-            $scope.$apply();
-            var duration = new Date() - start;
-            console.debug('msg-handler duration: ' + duration + ' ms');
-        });
-
+;
+(function() {
+    function setDummyTestResults($scope) {
         $scope.testResults = [{
             name: 'test_1',
             file: 'file_1.py',
@@ -43,7 +28,9 @@ app.controller('TestResultsTableController', ['$scope', 'WebSocketService',
             file: 'file_3.py',
             verdict: defs.Verdict.PASS
         }, ];
+    };
 
+    function setResultFilterSelections($scope) {
         $scope.resultFilterSelections = [{
             name: 'pass'
         }, {
@@ -53,13 +40,11 @@ app.controller('TestResultsTableController', ['$scope', 'WebSocketService',
         }, {
             name: 'all'
         }];
+    }
 
-        $scope.clearEvents = function() {
-            $scope.webSocketEvents = [];
-        }
-
-        var vc = util.countVerdicts($scope.testResults);
-        $scope.verdictCounts = vc;
+    function setPieData($scope) {
+        var vc = util.countVerdicts($scope.testResults)
+        $scope.verdictCounts = vc
 
         // TODO Chart experiment
         $scope.pieData = [{
@@ -79,10 +64,41 @@ app.controller('TestResultsTableController', ['$scope', 'WebSocketService',
         $scope.pieOptions = {
             animateRotate: false,
             legend: true,
-            legendTemplate : ['<ul class=\"<%=name.toLowerCase()%>-legend\">',
-            '<% for (var i=0; i<segments.length; i++){%>',
-            '<li><span style=\"background-color:<%=segments[i].fillColor%>\"></span>',
-            '<%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'].join('')
+            legendTemplate: ['<ul class=\"<%=name.toLowerCase()%>-legend\">',
+                '<% for (var i=0; i<segments.length; i++){%>',
+                '<li><span style=\"background-color:<%=segments[i].fillColor%>\"></span>',
+                '<%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+            ].join('')
         };
     }
-]);
+
+    app.controller('TestResultsTableController', ['$scope', 'WebSocketService',
+
+        function($scope, WebSocketService) {
+            $scope.webSocketEvents = [];
+            $scope.dummyPayload = WebSocketService.registerClient();
+
+            $scope.$on('webSocketMsg', function(event, data) {
+                var start = new Date()
+                console.debug('$scope: webSocketMsg', data);
+                $scope.webSocketEvents.push(data);
+                if ($scope.webSocketEvents.length > defs.maxDummyMsgScopeLength) {
+                    $scope.webSocketEvents = $scope.webSocketEvents.splice(1);
+                }
+                $scope.$apply()
+                var duration = new Date() - start;
+                console.debug(
+                    'msg-handler duration: ' + duration + ' ms');
+            });
+
+
+            $scope.clearEvents = function() {
+                $scope.webSocketEvents = [];
+            };
+
+            setDummyTestResults($scope);
+            setResultFilterSelections($scope);
+            setPieData($scope);
+        }
+    ]);
+})();
