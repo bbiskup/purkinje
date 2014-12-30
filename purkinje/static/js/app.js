aaa4f7c2121d223146d76c1ad0539a5abdd02d62
Bernhard Biskup <bbiskup@gmx.de>
2014-12-30 18:55:15 +0100

    Angular run block injection to facilitate testing

--- purkinje/static/js/app.js
+++ purkinje/static/js/app.js
@@ -13,14 +13,18 @@ app.config(['$interpolateProvider',
  * Timing of $digest
  * see http://www.rosher.co.uk/post/Angular-Tips-Measuring-Rendering-Performance
  */
-angular.module('purkinje').run(['$rootScope',
-    function($rootScope) {
-        var $oldDigest = $rootScope.$digest;
-        var $newDigest = function() {
-            console.time("$digest");
-            $oldDigest.apply($rootScope);
-            console.timeEnd("$digest");
-        }
-        $rootScope.$digest = $newDigest;
+angular.module('purkinje').run(wrapTimerWithDigest);
+
+wrapTimerWithDigest.$inject = ['$rootScope'];
+
+// Run block injection to facilitate testing
+// see https://github.com/johnpapa/angularjs-styleguide
+function wrapTimerWithDigest($rootScope) {
+    var $oldDigest = $rootScope.$digest;
+    var $newDigest = function() {
+        console.time("$digest");
+        $oldDigest.apply($rootScope);
+        console.timeEnd("$digest");
     }
-]);
+    $rootScope.$digest = $newDigest;
+}
