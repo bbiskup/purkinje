68a744bca1a600ffca23d19f52029002b92a47c8
Bernhard Biskup <bbiskup@gmx.de>
2014-12-30 18:17:01 +0100

    Include total test case count in verdict count

--- purkinje/static/js/util.js
+++ purkinje/static/js/util.js
@@ -3,8 +3,10 @@ window.util = {
      * Computes counts of verdicts to display summary
      */
     countVerdicts: function(testResults) {
-        return _.countBy(testResults, function(item) {
+        result =  _.countBy(testResults, function(item) {
             return item.verdict;
         });
+        result.all = testResults.length;
+        return result;
     }
 };
