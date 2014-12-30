3f5b1678cdfd555555d65c04b7ca9bcaadffcaf3
Bernhard Biskup <bbiskup@gmx.de>
2014-12-30 22:32:47 +0100

    filter as named function

--- purkinje/static/js/filters.js
+++ purkinje/static/js/filters.js
@@ -5,19 +5,22 @@
     /**
      * Gives CSS class for test verdict
      */
-    angular.module('purkinje').
-    filter('verdictClassFilter', function() {
-        return function(verdict, cssClass) {
-            switch (verdict) {
-                case defs.Verdict.PASS:
-                    return 'success';
-                case defs.Verdict.FAIL:
-                    return 'danger';
-                case defs.Verdict.ERROR:
-                    return 'warning';
-                default:
-                    return 'default';
-            };
+    angular
+        .module('purkinje')
+        .filter('verdictClassFilter', function() {
+            return verdictClassFilter;
+        });
+
+    function verdictClassFilter(verdict, cssClass) {
+        switch (verdict) {
+            case defs.Verdict.PASS:
+                return 'success';
+            case defs.Verdict.FAIL:
+                return 'danger';
+            case defs.Verdict.ERROR:
+                return 'warning';
+            default:
+                return 'default';
         };
-    });
+    };
 })();
