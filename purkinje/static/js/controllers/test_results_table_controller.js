a7a5a25ff19527adfbc0317cedc3a62c502679d0
Bernhard Biskup <bbiskup@gmx.de>
2014-12-30 22:22:47 +0100

    Define controller functions in closure

--- purkinje/static/js/controllers/test_results_table_controller.js
+++ purkinje/static/js/controllers/test_results_table_controller.js
@@ -2,110 +2,112 @@
 (function() {
     'use strict'
 
-    /**
-     * Choices for result filter combo box
-     */
-    function setResultFilterSelections($scope) {
-        $scope.resultFilterSelections = [{
-            name: 'pass'
-        }, {
-            name: 'fail & error'
-        }, {
-            name: 'skipped'
-        }, {
-            name: 'all'
-        }];
-    }
 
-    /**
-     * Configuration of verdict pie diagram
-     */
-    function setPieOptions($scope) {
-        $scope.pieOptions = {
-            animateRotate: false,
-            legend: true,
-            legendTemplate: ['<ul class=\"<%=name.toLowerCase()%>-legend\">',
-                '<% for (var i=0; i<segments.length; i++){%>',
-                '<li><span style=\"background-color:<%=segments[i].fillColor%>\"></span>',
-                '<%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
-            ].join('')
-        };
-    }
-
-    /*
-     * Set verdict pie diagram categories
-     */
-    function setPieData($scope) {
-        var vc = util.countVerdicts($scope.testResults);
-        $scope.verdictCounts = vc;
-
-        // TODO Chart experiment
-        $scope.pieData = [{
-            label: 'Pass',
-            value: vc.pass || 0,
-            color: 'green'
-        }, {
-            label: 'Fail',
-            value: vc.fail || 0,
-            color: 'red'
-        }, {
-            label: 'Error',
-            value: vc.error || 0,
-            color: '#D4CCC5'
-        }];
-    }
-
-    function handlews_sessionStarted($scope, data) {
-        $scope.testResults = [];
-    }
-
-    function handlews_tcFinished($scope, data) {
-        // $scope.testResults.push(data);
-        $scope.testResults.unshift(data);
-    }
-
-    function handlews_info($scope, data) {
-        $scope.info = data.id + ': ' + data.text;
-    }
-
-    /**
-     * Handle events from purkinje server
-     */
-    function handleWebSocketEvent($scope, event, data) {
-        var start = new Date();
-
-        data.forEach(function(msg) {
-            msg = JSON.parse(msg);
-            console.debug('$scope: webSocketMsg', msg);
-            var eventType = msg.type;
-            switch (eventType) {
-                case 'session_started':
-                    handlews_sessionStarted($scope, msg);
-                    break;
-                case 'tc_finished':
-                    handlews_tcFinished($scope, msg);
-                    break;
-                case 'info':
-                    handlews_info($scope, msg);
-                    break;
-                default:
-                    console.debug('Unsupported event type:', eventType);
-            };
-        });
-
-
-        setPieData($scope);
-        $scope.$apply();
-
-        var duration = new Date() - start;
-        console.debug(
-            'msg-handler duration: ' + duration + ' ms');
-    }
 
     angular.module('purkinje').controller('TestResultsTableController', ['$scope', 'WebSocketService', 'AvvisoService',
 
         function($scope, WebSocketService, AvvisoService) {
             //AvvisoService.notify('mytitle', 'mybody');
+
+            /**
+             * Choices for result filter combo box
+             */
+            function setResultFilterSelections() {
+                $scope.resultFilterSelections = [{
+                    name: 'pass'
+                }, {
+                    name: 'fail & error'
+                }, {
+                    name: 'skipped'
+                }, {
+                    name: 'all'
+                }];
+            }
+
+            /**
+             * Configuration of verdict pie diagram
+             */
+            function setPieOptions() {
+                $scope.pieOptions = {
+                    animateRotate: false,
+                    legend: true,
+                    legendTemplate: ['<ul class=\"<%=name.toLowerCase()%>-legend\">',
+                        '<% for (var i=0; i<segments.length; i++){%>',
+                        '<li><span style=\"background-color:<%=segments[i].fillColor%>\"></span>',
+                        '<%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+                    ].join('')
+                };
+            }
+
+            /*
+             * Set verdict pie diagram categories
+             */
+            function setPieData() {
+                var vc = util.countVerdicts($scope.testResults);
+                $scope.verdictCounts = vc;
+
+                // TODO Chart experiment
+                $scope.pieData = [{
+                    label: 'Pass',
+                    value: vc.pass || 0,
+                    color: 'green'
+                }, {
+                    label: 'Fail',
+                    value: vc.fail || 0,
+                    color: 'red'
+                }, {
+                    label: 'Error',
+                    value: vc.error || 0,
+                    color: '#D4CCC5'
+                }];
+            }
+
+            function handlews_sessionStarted(data) {
+                $scope.testResults = [];
+            }
+
+            function handlews_tcFinished(data) {
+                // $scope.testResults.push(data);
+                $scope.testResults.unshift(data);
+            }
+
+            function handlews_info(data) {
+                $scope.info = data.id + ': ' + data.text;
+            }
+
+            /**
+             * Handle events from purkinje server
+             */
+            function handleWebSocketEvent(event, data) {
+                var start = new Date();
+
+                data.forEach(function(msg) {
+                    msg = JSON.parse(msg);
+                    console.debug('$scope: webSocketMsg', msg);
+                    var eventType = msg.type;
+                    switch (eventType) {
+                        case 'session_started':
+                            handlews_sessionStarted(msg);
+                            break;
+                        case 'tc_finished':
+                            handlews_tcFinished(msg);
+                            break;
+                        case 'info':
+                            handlews_info(msg);
+                            break;
+                        default:
+                            console.debug('Unsupported event type:', eventType);
+                    };
+                });
+
+
+                setPieData($scope);
+                $scope.$apply();
+
+                var duration = new Date() - start;
+                console.debug(
+                    'msg-handler duration: ' + duration + ' ms');
+            }
 
 
             $scope.testResults = [];
@@ -113,18 +115,18 @@
             $scope.dummyPayload = WebSocketService.registerClient();
 
             $scope.$on('webSocketMsg', function(event, data) {
-                handleWebSocketEvent($scope, event, data);
+                handleWebSocketEvent(event, data);
             });
 
 
             $scope.clearEvents = function() {
                 $scope.testResults = [];
-                setPieData($scope);
+                setPieData();
             };
 
             //setDummyTestResults($scope);
-            setResultFilterSelections($scope);
-            setPieOptions($scope);
+            setResultFilterSelections();
+            setPieOptions();
         }
     ]);
 })();
