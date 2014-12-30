99dea3515631505745a3b83005157950aa2a03e1
Bernhard Biskup <bbiskup@gmx.de>
2014-12-30 18:18:25 +0100

    Wrap angular $digest to measure its time

--- purkinje/static/js/app.js
+++ purkinje/static/js/app.js
@@ -1,8 +1,26 @@
 var app = angular.module('purkinje', ['ui.bootstrap', 'tc.chartjs']);
 //var app = angular.module('purkinje', []);
 
-app.config(['$interpolateProvider', function($interpolateProvider) {
-    $interpolateProvider.startSymbol('{[');
-    $interpolateProvider.endSymbol(']}');
-}]);
+app.config(['$interpolateProvider',
+    function($interpolateProvider) {
+        $interpolateProvider.startSymbol('{[');
+        $interpolateProvider.endSymbol(']}');
+    }
+]);
 
+
+/**
+ * Timing of $digest
+ * see http://www.rosher.co.uk/post/Angular-Tips-Measuring-Rendering-Performance
+ */
+angular.module('purkinje').run(['$rootScope',
+    function($rootScope) {
+        var $oldDigest = $rootScope.$digest;
+        var $newDigest = function() {
+            console.time("$digest");
+            $oldDigest.apply($rootScope);
+            console.timeEnd("$digest");
+        }
+        $rootScope.$digest = $newDigest;
+    }
+]);
