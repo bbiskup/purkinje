5e0b934d4ba89b9a9934a37c3d2342855ca7c2b8
Bernhard Biskup <bbiskup@gmx.de>
2015-01-05 18:10:49 +0100

    Use angular ui-router

---
+++ purkinje/static/js/routes.js
@@ -0,0 +1,28 @@
+;
+(function() {
+    'use strict';
+    angular.module('purkinje').config(['$stateProvider', '$urlRouterProvider',
+        configRouting
+    ]);
+
+    function configRouting($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/dashboard');
+
+        var tmplPrefix = '/static/templates';
+
+        $stateProvider
+            .state('dashboard', {
+                url: '/dashboard',
+                templateUrl: tmplPrefix + '/partial-dashboard.html'
+            })
+            .state('settings', {
+                url: '/settings',
+                templateUrl: tmplPrefix + '/partial-settings.html'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: tmplPrefix + '/partial-about.html'
+            });
+    }
+
+})();
