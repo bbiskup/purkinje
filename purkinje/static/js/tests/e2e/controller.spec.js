5aaf98c624bd6179dea42475b7c93d2b41e02b15
Bernhard Biskup <bbiskup@gmx.de>
2015-01-05 20:06:27 +0100

    Formatting

--- purkinje/static/js/tests/e2e/controller.spec.js
+++ purkinje/static/js/tests/e2e/controller.spec.js
@@ -12,16 +12,16 @@ describe('E2E: test 1', function() {
         expect('1').toBe('1');
     });
 
-it("Dummy test without DOM", function() {
-    expect(3).toEqual(3);
-});
+    it("Dummy test without DOM", function() {
+        expect(3).toEqual(3);
+    });
 
     it("Dummy DOM test", function() {
         //expect(element(by.css('#test-results-header'))).getText().toBe('Test Results');
         expect(browser.getTitle()).toEqual('Purkinje Test Runner');
     });
 
-    
+
     var selector = '#result-summary';
     it("should have the title set", function() {
         expect(browser.getTitle()).toEqual('Purkinje Test Runner');
