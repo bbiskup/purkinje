30c238b559d90cadb7976a8b863291f8e270dd5e
Bernhard Biskup <bbiskup@gmx.de>
2015-01-05 18:12:58 +0100

    Use ui-router; dedicated navbar controller

---
+++ purkinje/static/js/controllers/navbar_controller.js
@@ -0,0 +1,22 @@
+;
+(function() {
+    'use strict';
+
+    angular
+        .module('purkinje')
+        .controller('NavBarController', ['$scope', '$state', NavBarController]);
+
+
+    /**
+    * This controller handles the state of the navigation bar.
+    * 
+    * It gives access to the current ui-router routing state to
+    * highlight the corresponding navigation menu item
+    */
+    function NavBarController($scope, $state) {
+
+        $scope.isActive = function(stateName){
+            return $state.current.name === stateName;
+        }
+    }
+})();
