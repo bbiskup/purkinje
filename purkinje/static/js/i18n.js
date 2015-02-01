631a349d8813ecb5616a0c35e1e59dc09f195ac9
Bernhard Biskup <bbiskup@gmx.de>
2015-02-01 15:08:43 +0100

    i18n for settings

--- purkinje/static/js/i18n.js
+++ purkinje/static/js/i18n.js
@@ -4,7 +4,12 @@
         .module('purkinje')
         .config(function($translateProvider) {
             $translateProvider.translations('en', {
+
+                // Settings page
                 SETTINGS_HEADLINE: 'Settings',
+                HOST: 'Host',
+                DIRECTORY: 'Directory',
+                USER: 'User',
 
                 // About page
                 ABOUT_HEADLINE: 'About purkinje',
@@ -13,7 +18,11 @@
                 HIDE_LICENSE_BUTTON_TEXT: 'Hide license'
             });
             $translateProvider.translations('de', {
+                // Settings page
                 SETTINGS_HEADLINE: 'Einstellungen',
+                HOST: 'Host',
+                DIRECTORY: 'Verzeichnis',
+                USER: 'Benutzer',
 
                 // About page
                 ABOUT_HEADLINE: 'Über purkinje',
