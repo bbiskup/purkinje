b3e17280c34a45481d436dc6ec7c041b6a7b12f1
Bernhard Biskup <bbiskup@gmx.de>
2015-02-01 15:53:48 +0100

    More i18n

--- purkinje/static/js/i18n.js
+++ purkinje/static/js/i18n.js
@@ -4,6 +4,18 @@
         .module('purkinje')
         .config(function($translateProvider) {
             $translateProvider.translations('en', {
+                // Dashboard
+                NO_TEST_SUITE: 'No test suite',
+                PASS: 'Pass',
+                FAILED: 'Failed',
+                ERRORS: 'Errors',
+                SKIPPED: 'Skipped',
+                FAST: 'Fast',
+                NORMAL: 'Normal',
+                SLOW: 'Slow',
+                SLOWEST: 'Slowest',
+                CLEAR_BUTTON: 'Clear',
+                DUMMY_DATA_BUTTON: 'Dummy data',
 
                 // Settings page
                 SETTINGS_HEADLINE: 'Settings',
@@ -15,9 +27,27 @@
                 ABOUT_HEADLINE: 'About purkinje',
                 PURKINJE_ON_GITHUB: 'purkinje on GitHub',
                 SHOW_LICENSE_BUTTON_TEXT: 'Show license',
-                HIDE_LICENSE_BUTTON_TEXT: 'Hide license'
+                HIDE_LICENSE_BUTTON_TEXT: 'Hide license',
+
+                // Activity state directive
+                STATE: 'State',
+                RUNNING: 'Running...',
+                IDLE: 'Idle'
             });
             $translateProvider.translations('de', {
+                // Dashboard
+                NO_TEST_SUITE: 'Keine Testsuite',
+                PASS: 'Bestanden',
+                FAILED: 'Fehlgeschlagen',
+                ERRORS: 'Fehler',
+                SKIPPED: 'Übersprungen',
+                FAST: 'Schnell',
+                NORMAL: 'Normal',
+                SLOW: 'Langsam',
+                SLOWEST: 'Sehr langsam',
+                CLEAR_BUTTON: 'Löschen',
+                DUMMY_DATA_BUTTON: 'Dummy-Daten',
+
                 // Settings page
                 SETTINGS_HEADLINE: 'Einstellungen',
                 HOST: 'Host',
@@ -28,7 +58,12 @@
                 ABOUT_HEADLINE: 'Über purkinje',
                 PURKINJE_ON_GITHUB: 'purkinje auf GitHub',
                 SHOW_LICENSE_BUTTON_TEXT: 'Lizenz anzeigen',
-                HIDE_LICENSE_BUTTON_TEXT: 'Lizenz verbergen'
+                HIDE_LICENSE_BUTTON_TEXT: 'Lizenz verbergen',
+
+                // Activity state directive
+                STATE: 'Zustand',
+                RUNNING: 'läuft...',
+                IDLE: 'inaktiv'
             });
             // $translateProvider.preferredLanguage('en');
             $translateProvider.determinePreferredLanguage();
