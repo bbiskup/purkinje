65b4d99431090efa1964ed2f470af40bcaf2ce9a
Bernhard Biskup <bbiskup@gmx.de>
2015-02-08 17:24:59 +0100

    Added protractor tests

--- purkinje/static/js/tests/e2e/controller.spec.js
+++ purkinje/static/js/tests/e2e/controller.spec.js
@@ -4,7 +4,7 @@
     describe('E2E: test 1', function() {
         beforeEach(function() {
             // TODO actual E2E test URL
-            browser.get('http://localhost:5000/');
+            browser.get('/');
             //browser().reload();
         });
 
@@ -17,15 +17,18 @@
             expect(3).toEqual(3);
         });
 
-        it('Dummy DOM test', function() {
-            //expect(element(by.css('#test-results-header'))).getText().toBe('Test Results');
+        /*it('Dummy DOM test', function() {
+            expect(element(by.css('#test-results-header'))).getText().toBe('Test Results');
+        });*/
+
+        it('should have the title set', function() {
             expect(browser.getTitle()).toEqual('Purkinje Test Runner');
         });
 
-
-        var selector = '#result-summary';
-        it('should have the title set', function() {
-            expect(browser.getTitle()).toEqual('Purkinje Test Runner');
+        it('Initially no test suite', function() {
+            var elem = element(by.css('#test-suite-header'));
+            expect(elem.getText()).toBe('No test suite');
+            // expect(elem).toHaveClass('testsuite-header-inactive');
         });
     });
 })();
