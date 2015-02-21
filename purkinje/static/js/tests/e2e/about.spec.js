a6ed6c6144a4eb7d13810f75a484f6b34d6f16d0
Bernhard Biskup <bbiskup@gmx.de>
2015-02-21 20:05:42 +0100

    Simplified about page; added E2E tests for about page

---
+++ purkinje/static/js/tests/e2e/about.spec.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+
+    describe('About page', function() {
+        beforeEach(function() {
+            browser.get('/#/about')
+        });
+
+        it('Github link present', function() {
+            element.all(by.id('purkinje-github-link')).count().then(function(theCount) {
+                expect(theCount).toBeGreaterThan(0);
+            });
+        });
+
+        it('License info present', function() {
+            element.all(by.css('.license-info')).count().then(function(theCount) {
+                expect(theCount).toBeGreaterThan(0);
+            });
+        });
+    });
+})();
