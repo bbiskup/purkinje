3fb535861a13f8e472345ac2cbca783f08786cf5
Bernhard Biskup <bbiskup@gmx.de>
2015-02-21 20:15:11 +0100

    Added E2E tests for settings page

---
+++ purkinje/static/js/tests/e2e/settings.spec.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+
+    describe('Settings page', function() {
+        beforeEach(function() {
+            browser.get('/#/settings')
+        });
+
+        it('Headline set', function() {
+            var elem = element(by.id('settings-headline'));
+            expect(elem.getText()).toBe('Settings');
+        });
+
+        it('Settings table present', function() {
+            element(by.id('settings-settings-table')).isPresent();
+        });
+    });
+})();
