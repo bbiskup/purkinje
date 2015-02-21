2cfa5b656504339b7896ef1ed9d34c12387c423a
Bernhard Biskup <bbiskup@gmx.de>
2015-02-21 20:06:12 +0100

    Simplified about page and added E2E tests

--- purkinje/static/js/controllers/about_controller.js
+++ purkinje/static/js/controllers/about_controller.js
@@ -6,16 +6,5 @@
         .controller('AboutController', ['$scope', AboutController]);
 
     function AboutController($scope) {
-        $scope.isLicenseCollapsed = true;
-
-        $scope.licenseToggleText = function() {
-            switch ($scope.isLicenseCollapsed) {
-                case true:
-                    return 'SHOW_LICENSE_BUTTON_TEXT';
-                case false:
-                    return 'HIDE_LICENSE_BUTTON_TEXT';
-                default: break;
-            }
-        };
     }
 })();
