f0634520cdd42ba8ed82bbce38fade135eddf122
Bernhard Biskup <bbiskup@gmx.de>
2016-05-14 20:13:15 +0200

    Version bump 0.1.8 --> 0.1.9

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class Tox(cmd_test):
         sys.exit(errcode)
 
 setup(name='purkinje',
-      version='0.1.8',
+      version='0.1.9',
       description='Test runner for py.test with web GUI',
       long_description=readme + '\n\n' + changes,
       author='Bernhard Biskup',
