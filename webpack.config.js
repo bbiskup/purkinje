cd6bd216e26f3089e64c4dbef22c9db01ee06e3a
Bernhard Biskup <bbiskup@gmx.de>
2016-05-29 17:54:11 +0200

    Added npm packages; upgraded package versions; removed old webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './purkinje/static/js/app',
+    devServer: {
+        contentBase: "./purkinje/static/"
+    },
     output: {
         path: 'dist',
         filename: 'bundle.js'
