18d894a21f0170bf3f88db5abaccee9eb2287f5a
Bernhard Biskup <bernhard@fract-tech.com>
2017-04-22 02:07:40 +0200

    Replaced obsolete protractor option 'chromeOnly' with 'directConnect'

--- meta/js/protractor-conf.js
+++ meta/js/protractor-conf.js
@@ -1,29 +1,27 @@
 (function() {
 
-    'use strict';
+  'use strict';
 
-    // protractor configuration
+  // protractor configuration
 
-    exports.config = {
-        //seleniumAddress: 'http://localhost:4444/wd/hub',
-        specs: ['purkinje/static/js/tests/e2e/*.spec.js'],
+  exports.config = {
+    // seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['purkinje/static/js/tests/e2e/*.spec.js'],
 
-        baseUrl: 'http://purkinje:5000/',
+    baseUrl: 'http://purkinje:5000/',
 
-        capabilities: {
-            // Selenium
-            // "Selenium 2.44 is currently not compatible with Firefox 35. –  rajana sekhar Feb 3 at 7:18"
-            // http://stackoverflow.com/a/28107139
-            //browserName: 'firefox',
-            browserName: 'chrome',
-        },
-        //directConnect: true,
-        chromeOnly: true,
+    capabilities: {
+      // Selenium
+      // "Selenium 2.44 is currently not compatible with Firefox 35. –  rajana
+      // sekhar Feb 3 at 7:18"
+      // http://stackoverflow.com/a/28107139
+      // browserName: 'firefox',
+      browserName: 'chrome',
+    },
+    directConnect: true,
+    // chromeOnly: true,
 
-        jasmineNodeOpts: {
-            isVerbose: true,
-            showColors: true,
-            includeStackTrace: true
-        }
-    };
+    jasmineNodeOpts:
+        {isVerbose: true, showColors: true, includeStackTrace: true}
+  };
 })();
